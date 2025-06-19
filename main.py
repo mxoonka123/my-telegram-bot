d7f1e44cde3bfce5ed14f762816c490c49355ce3
Maxim <maximka17204@gmail.com>
2025-06-19 22:59:28 +0300

    fix: Handle asyncio event loop in bot thread

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import threading
 from waitress import serve
 import os
 from datetime import timedelta
+import json
+import asyncio
 
 # --- Настройка логирования в самом начале ---
 logging.basicConfig(
@@ -120,76 +122,91 @@ async def post_init(application: Application):
 
 
 def run_telegram_bot():
-    """Собирает и запускает инстанс бота."""
-    logger.info("--- Preparing Telegram Bot ---")
+    """Собирает и запускает инстанс бота в новом цикле событий asyncio."""
+    logger.info("--- Preparing Telegram Bot Thread ---")
 
-    application = (
-        Application.builder()
-        .token(config.TELEGRAM_TOKEN)
-        .defaults(Defaults(parse_mode=ParseMode.MARKDOWN_V2, block=False))
-        .pool_timeout(20.0)
-        .connect_timeout(20.0)
-        .read_timeout(30.0)
-        .write_timeout(30.0)
-        .connection_pool_size(50)
-        .post_init(post_init)
-        .build()
-    )
+    # 1. Создаем новый цикл событий для этого потока
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    
+    logger.info("New event loop created and set for BotThread.")
 
-    # ... (ЗДЕСЬ ВСЯ РЕГИСТРАЦИЯ ХЕНДЛЕРОВ ИЗ ВАШЕГО RUN_BOT.PY) ...
-    # Я скопирую для полноты картины
-    edit_persona_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('editpersona', handlers.edit_persona_start), CallbackQueryHandler(handlers.edit_persona_button_callback, pattern='^edit_persona_')],
-        states={
-            handlers.EDIT_WIZARD_MENU: [CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^edit_wizard_|^finish_edit$|^back_to_wizard_menu$|^set_max_msgs_')],
-            handlers.EDIT_NAME: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_name_received), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
-            handlers.EDIT_DESCRIPTION: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_description_received), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
-            handlers.EDIT_COMM_STYLE: [CallbackQueryHandler(handlers.edit_comm_style_received, pattern='^set_comm_style_|^back_to_wizard_menu$')],
-            handlers.EDIT_VERBOSITY: [CallbackQueryHandler(handlers.edit_verbosity_received, pattern='^set_verbosity_|^back_to_wizard_menu$')],
-            handlers.EDIT_GROUP_REPLY: [CallbackQueryHandler(handlers.edit_group_reply_received, pattern='^set_group_reply_|^back_to_wizard_menu$')],
-            handlers.EDIT_MEDIA_REACTION: [CallbackQueryHandler(handlers.edit_media_reaction_received, pattern='^set_media_react_|^back_to_wizard_menu$')],
-            handlers.EDIT_MOODS_ENTRY: [CallbackQueryHandler(handlers.edit_moods_entry, pattern='^edit_wizard_moods$'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
-            handlers.EDIT_MOOD_CHOICE: [CallbackQueryHandler(handlers.edit_mood_choice, pattern='^editmood_|^deletemood_confirm_|^editmood_add$'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$'), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
-            handlers.EDIT_MOOD_NAME: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_mood_name_received), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
-            handlers.EDIT_MOOD_PROMPT: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_mood_prompt_received), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
-            handlers.DELETE_MOOD_CONFIRM: [CallbackQueryHandler(handlers.delete_mood_confirmed, pattern='^deletemood_delete_'), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
-            handlers.EDIT_MAX_MESSAGES: [CallbackQueryHandler(handlers.edit_max_messages_received, pattern='^set_max_msgs_'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
-        },
-        fallbacks=[CommandHandler('cancel', handlers.edit_persona_cancel), CallbackQueryHandler(handlers.edit_persona_finish, pattern='^finish_edit$'), CallbackQueryHandler(handlers.edit_persona_cancel, pattern='^cancel_wizard$')],
-        per_message=False, name="edit_persona_wizard", conversation_timeout=timedelta(minutes=15).total_seconds(), allow_reentry=True
-    )
-    delete_persona_conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('deletepersona', handlers.delete_persona_start), CallbackQueryHandler(handlers.delete_persona_button_callback, pattern=r'^delete_persona_\d+$')],
-        states={handlers.DELETE_PERSONA_CONFIRM: [CallbackQueryHandler(handlers.delete_persona_confirmed, pattern='^delete_persona_confirm_'), CallbackQueryHandler(handlers.delete_persona_cancel, pattern='^delete_persona_cancel$')]},
-        fallbacks=[CommandHandler('cancel', handlers.delete_persona_cancel), CallbackQueryHandler(handlers.delete_persona_cancel, pattern='^delete_persona_cancel$')],
-        per_message=False, name="delete_persona_conversation", conversation_timeout=timedelta(minutes=5).total_seconds(), allow_reentry=True
-    )
+    # 2. Вся остальная логика выполняется внутри этого цикла
+    try:
+        application = (
+            Application.builder()
+            .token(config.TELEGRAM_TOKEN)
+            .defaults(Defaults(parse_mode=ParseMode.MARKDOWN_V2, block=False))
+            .pool_timeout(20.0)
+            .connect_timeout(20.0)
+            .read_timeout(30.0)
+            .write_timeout(30.0)
+            .connection_pool_size(50)
+            .post_init(post_init)
+            .build()
+        )
 
-    application.add_handler(edit_persona_conv_handler)
-    application.add_handler(delete_persona_conv_handler)
-    application.add_handler(CommandHandler("start", handlers.start))
-    application.add_handler(CommandHandler("help", handlers.help_command))
-    application.add_handler(CommandHandler("menu", handlers.menu_command))
-    application.add_handler(CommandHandler("profile", handlers.profile))
-    application.add_handler(CommandHandler("subscribe", handlers.subscribe))
-    application.add_handler(CommandHandler("createpersona", handlers.create_persona))
-    application.add_handler(CommandHandler("mypersonas", handlers.my_personas))
-    application.add_handler(CommandHandler("addbot", handlers.add_bot_to_chat))
-    application.add_handler(CommandHandler("mood", handlers.mood))
-    application.add_handler(CommandHandler("reset", handlers.reset))
-    application.add_handler(CommandHandler("clear", handlers.reset))
-    application.add_handler(CommandHandler("mutebot", handlers.mute_bot))
-    application.add_handler(CommandHandler("unmutebot", handlers.unmute_bot))
-    application.add_handler(MessageHandler(handlers.filters.PHOTO & ~handlers.filters.COMMAND, handlers.handle_photo))
-    application.add_handler(MessageHandler(handlers.filters.VOICE & ~handlers.filters.COMMAND, handlers.handle_voice))
-    application.add_handler(MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.handle_message))
-    application.add_handler(CallbackQueryHandler(handlers.handle_callback_query))
-    application.add_error_handler(handlers.error_handler)
+        edit_persona_conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('editpersona', handlers.edit_persona_start), CallbackQueryHandler(handlers.edit_persona_button_callback, pattern='^edit_persona_')],
+            states={
+                handlers.EDIT_WIZARD_MENU: [CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^edit_wizard_|^finish_edit$|^back_to_wizard_menu$|^set_max_msgs_')],
+                handlers.EDIT_NAME: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_name_received), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
+                handlers.EDIT_DESCRIPTION: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_description_received), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
+                handlers.EDIT_COMM_STYLE: [CallbackQueryHandler(handlers.edit_comm_style_received, pattern='^set_comm_style_|^back_to_wizard_menu$')],
+                handlers.EDIT_VERBOSITY: [CallbackQueryHandler(handlers.edit_verbosity_received, pattern='^set_verbosity_|^back_to_wizard_menu$')],
+                handlers.EDIT_GROUP_REPLY: [CallbackQueryHandler(handlers.edit_group_reply_received, pattern='^set_group_reply_|^back_to_wizard_menu$')],
+                handlers.EDIT_MEDIA_REACTION: [CallbackQueryHandler(handlers.edit_media_reaction_received, pattern='^set_media_react_|^back_to_wizard_menu$')],
+                handlers.EDIT_MOODS_ENTRY: [CallbackQueryHandler(handlers.edit_moods_entry, pattern='^edit_wizard_moods$'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
+                handlers.EDIT_MOOD_CHOICE: [CallbackQueryHandler(handlers.edit_mood_choice, pattern='^editmood_|^deletemood_confirm_|^editmood_add$'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$'), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
+                handlers.EDIT_MOOD_NAME: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_mood_name_received), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
+                handlers.EDIT_MOOD_PROMPT: [MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.edit_mood_prompt_received), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
+                handlers.DELETE_MOOD_CONFIRM: [CallbackQueryHandler(handlers.delete_mood_confirmed, pattern='^deletemood_delete_'), CallbackQueryHandler(handlers.edit_mood_choice, pattern='^edit_moods_back_cancel$')],
+                handlers.EDIT_MAX_MESSAGES: [CallbackQueryHandler(handlers.edit_max_messages_received, pattern='^set_max_msgs_'), CallbackQueryHandler(handlers.edit_wizard_menu_handler, pattern='^back_to_wizard_menu$')],
+            },
+            fallbacks=[CommandHandler('cancel', handlers.edit_persona_cancel), CallbackQueryHandler(handlers.edit_persona_finish, pattern='^finish_edit$'), CallbackQueryHandler(handlers.edit_persona_cancel, pattern='^cancel_wizard$')],
+            per_message=False, name="edit_persona_wizard", conversation_timeout=timedelta(minutes=15).total_seconds(), allow_reentry=True
+        )
+        delete_persona_conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('deletepersona', handlers.delete_persona_start), CallbackQueryHandler(handlers.delete_persona_button_callback, pattern=r'^delete_persona_\d+$')],
+            states={handlers.DELETE_PERSONA_CONFIRM: [CallbackQueryHandler(handlers.delete_persona_confirmed, pattern='^delete_persona_confirm_'), CallbackQueryHandler(handlers.delete_persona_cancel, pattern='^delete_persona_cancel$')]}, 
+            fallbacks=[CommandHandler('cancel', handlers.delete_persona_cancel), CallbackQueryHandler(handlers.delete_persona_cancel, pattern='^delete_persona_cancel$')],
+            per_message=False, name="delete_persona_conversation", conversation_timeout=timedelta(minutes=5).total_seconds(), allow_reentry=True
+        )
 
-    logger.info("Handlers registered for Telegram bot.")
-    logger.info("Starting bot polling...")
-    application.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True, timeout=30)
-    logger.info("----- Bot polling stopped. -----")
+        application.add_handler(edit_persona_conv_handler)
+        application.add_handler(delete_persona_conv_handler)
+        application.add_handler(CommandHandler("start", handlers.start))
+        application.add_handler(CommandHandler("help", handlers.help_command))
+        application.add_handler(CommandHandler("menu", handlers.menu_command))
+        application.add_handler(CommandHandler("profile", handlers.profile))
+        application.add_handler(CommandHandler("subscribe", handlers.subscribe))
+        application.add_handler(CommandHandler("createpersona", handlers.create_persona))
+        application.add_handler(CommandHandler("mypersonas", handlers.my_personas))
+        application.add_handler(CommandHandler("addbot", handlers.add_bot_to_chat))
+        application.add_handler(CommandHandler("mood", handlers.mood))
+        application.add_handler(CommandHandler("reset", handlers.reset))
+        application.add_handler(CommandHandler("clear", handlers.reset))
+        application.add_handler(CommandHandler("mutebot", handlers.mute_bot))
+        application.add_handler(CommandHandler("unmutebot", handlers.unmute_bot))
+        application.add_handler(MessageHandler(handlers.filters.PHOTO & ~handlers.filters.COMMAND, handlers.handle_photo))
+        application.add_handler(MessageHandler(handlers.filters.VOICE & ~handlers.filters.COMMAND, handlers.handle_voice))
+        application.add_handler(MessageHandler(handlers.filters.TEXT & ~handlers.filters.COMMAND, handlers.handle_message))
+        application.add_handler(CallbackQueryHandler(handlers.handle_callback_query))
+        application.add_error_handler(handlers.error_handler)
+
+        logger.info("Handlers registered for Telegram bot.")
+        logger.info("Starting bot polling in the new event loop...")
+        
+        # 3. Запускаем polling через loop.run_until_complete()
+        loop.run_until_complete(application.run_polling(allowed_updates=Update.ALL_TYPES, drop_pending_updates=True, timeout=30))
+
+    except Exception as e:
+        logger.critical(f"An exception occurred in the bot thread's main logic: {e}", exc_info=True)
+    finally:
+        logger.info("Closing the event loop for BotThread.")
+        loop.close()
+
+    logger.info("----- Bot thread has finished. -----")
 
 
 if __name__ == '__main__':
